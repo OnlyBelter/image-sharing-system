d354083c7d91f7ea9e5f8a5786afcd3f46a2a506
OnlyBelter <xiongxin20008@126.com>
2017-08-07 16:20:38 +0800

    dashboard sort users by image number

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -17,7 +17,22 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.userService.getUsersByHttp()
-         .then(rep => this.users = rep.slice(1, 5));
+         .then(rep => this.users = this.getTopX(rep, 4));
+        //  .then(rep => console.log(rep));
   }
 
+  // 得到分享图片最多的前x个user
+  getTopX(users: User[], x: number) {
+    users.sort(function(a: User, b: User) {
+        if (a.files.length > b.files.length) {
+          return -1;
+        }
+        if (a.files.length < b.files.length) {
+          return 1;
+        }
+        return 0;
+    })
+      console.log(users);
+    return users.slice(0, x);
+    };
 }
